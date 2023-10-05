5bfb9de36d95ccb3b2a402598247547be80299aa
Amritha-07 <p.p.amrithakumar@gmail.com>
2023-10-05 14:09:40 +0530

    Login and Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <></>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <Routes>
+                    <Route exact='true' path='/' element={<Login />}></Route>
+                    <Route path='/home' element={[<Header />, <Home />]}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/LeftSide.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const LeftSide = () => {
+    return (
+        <Container>
+            LeftSide
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    grid-area: leftside;
+`
+
+export default LeftSide
